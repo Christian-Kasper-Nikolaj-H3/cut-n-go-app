f4e4ade7f364ba5b41811abbc05003238927bd7e
Kasper Pedersen <kasperpedersen1999@gmail.com>
2026-03-20 09:54:24 +0100

    feat(database): enhance associations for user and booking models
    Added associations between Users, Employees, Salons, and Bookings to improve data relationships and integrity within the database schema. This includes linking user roles, employee profiles, and booking information to their respective entities.

--- server/models/Associations.js
+++ server/models/Associations.js
@@ -1,5 +1,37 @@
-
+import UserRoles from "./UserRoles.js";
+import Users from "./Users.js";
+import UserInformation from "./UserInformation.js";
+import EmployeeRoles from "./EmployeeRoles.js";
+import Employees from "./Employees.js";
+import Salon from "./Salon.js";
+import Bookings from "./Bookings.js";
+import BookingInformation from "./BookingInformation.js";
 
 export default function setupAssociations() {
+    UserRoles.hasMany(Users, { foreignKey: "role_id", as: "users" });
+    Users.belongsTo(UserRoles, { foreignKey: "role_id", as: "role" });
 
+    Users.hasOne(UserInformation, { foreignKey: "user_id", as: "information" });
+    UserInformation.belongsTo(Users, { foreignKey: "user_id", as: "user" });
+
+    EmployeeRoles.hasMany(Employees, { foreignKey: "role_id", as: "employees" });
+    Employees.belongsTo(EmployeeRoles, { foreignKey: "role_id", as: "role" });
+
+    Salon.hasMany(Employees, { foreignKey: "salon_id", as: "employees" });
+    Employees.belongsTo(Salon, { foreignKey: "salon_id", as: "salon" });
+
+    Users.hasOne(Employees, { foreignKey: "user_id", as: "employeeProfile" });
+    Employees.belongsTo(Users, { foreignKey: "user_id", as: "user" });
+
+    Salon.hasMany(Bookings, { foreignKey: "salon_id", as: "bookings" });
+    Bookings.belongsTo(Salon, { foreignKey: "salon_id", as: "salon" });
+
+    Employees.hasMany(Bookings, { foreignKey: "employee_id", as: "bookings" });
+    Bookings.belongsTo(Employees, { foreignKey: "employee_id", as: "employee" });
+
+    Bookings.hasOne(BookingInformation, { foreignKey: "booking_id", as: "information" });
+    BookingInformation.belongsTo(Bookings, { foreignKey: "booking_id", as: "booking" });
+
+    Users.hasMany(BookingInformation, { foreignKey: "user_id", as: "bookingInformation" });
+    BookingInformation.belongsTo(Users, { foreignKey: "user_id", as: "user" });
 }
